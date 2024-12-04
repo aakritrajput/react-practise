5e3374f7cfa4123470292fe14b07aa3d15619e70
aakritrajput <aakritrajput87@gmail.com>
2024-12-04 20:37:26 +0530

    mega project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>hiii </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,13 @@
+//production grade app step to avoid erros like ids can be in the form of numbers and env variables should always be in strings 
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+
+
+export default conf
