fa76e2f86ba277d697e3d97d0e414372dc1e7a0a
aakritrajput <aakritrajput87@gmail.com>
2024-12-06 18:26:09 +0530

    mega project

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,5 +1,7 @@
-import conf from '../conf.js';
+/* eslint-disable no-useless-catch */
+import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
@@ -7,29 +9,27 @@ export class AuthService {
 
     constructor() {
         this.client
-          .setEndpoint(conf.appwriteUrl)
-          .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client)
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
     }
 
-    async createAccount({email, password, name}){
-        // eslint-disable-next-line no-useless-catch
+    async createAccount({email, password, name}) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
-                //call another method
+                // call another method
                 return this.login({email, password});
-                
             } else {
-                return userAccount;
+               return  userAccount;
             }
         } catch (error) {
             throw error;
         }
     }
 
-    async login({email,password}) {
-        // eslint-disable-next-line no-useless-catch
+    async login({email, password}) {
         try {
             return await this.account.createEmailPasswordSession(email, password);
         } catch (error) {
@@ -41,21 +41,20 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error)
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
 
         return null;
     }
 
     async logout() {
+
         try {
-            await this.account.deleteSessions()
+            await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error)
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
-
-
 }
 
 const authService = new AuthService();
--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,5 +1,3 @@
-//production grade app step to avoid erros like ids can be in the form of numbers and env variables should always be in strings 
-
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
@@ -7,7 +5,6 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
-
-
+// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
 export default conf
